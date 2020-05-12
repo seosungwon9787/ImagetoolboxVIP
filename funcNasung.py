3889321dcbe4ebd6f502fa764c910b7f7165e5e6
kwonnaseong <kwonnasung98@gmail.com>
2020-05-12 12:01:59 +0900

    Modify Grayscale error
    그레이스케일 후 초록색으로 변환되는 오류 해결

--- funcNasung.py
+++ funcNasung.py
@@ -8,13 +8,16 @@ import numpy as np
 
 def EdgeDetection(image):
     image_array = qimage2ndarray.rgb_view(image) #Qimage를 numpy로 변환
-    print(image_array)
+    print(image_array) #그레이 스케일 전 
     print(image_array.shape)
     #rgb를 Grayscale로 변환하는 공식 
-    arr=np.array([0.299,0.587,0.114]) 
-    image_array=arr*image_array #rgb값을 저장하는 배열에 공식을 대입함 
-    print(image_array)
-    image=qimage2ndarray.array2qimage(image_array, normalize=False) #numpy를 Qimage로 변환
+    arr=[0.299, 0.587, 0.114]
+    GrayScale=image_array.dot(arr)
+    print(GrayScale) #그레이 스케일 후 
+
+    #가우시안 필터
+
+    image=qimage2ndarray.array2qimage(GrayScale, normalize=False) #numpy를 Qimage로 변환
     qPixmapVar = QPixmap.fromImage(image) #Qimage를 Qpixmap으로 변환   
     
     return qPixmapVar
