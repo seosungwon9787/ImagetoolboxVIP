c9bf5a7a0ac8b03c81c1f080f4c7f1d26370848d
KNH <nh2004@naver.com>
2020-05-20 02:40:23 +0900

    laplacian 필터 추가
    응답없음은 에러 아님 로딩기다릴 것.
    너무 오래걸리면 scale=3, laplacian_mask1 사용

--- funcNahyun.py
+++ funcNahyun.py
@@ -5,7 +5,7 @@ import qimage2ndarray
 import numpy as np
 from numpy import pi, exp, sqrt
 
-
+#가우시안 마스크
 def gaussian(scale, sigma):
     mask = np.ones(shape=(scale, scale))
     sum = 0
@@ -16,6 +16,30 @@ def gaussian(scale, sigma):
             sum += mask[i, j]
     mask = mask / sum
     return mask
+
+#필터적용
+def filter(mask, scale, image):
+    # 패딩
+    npad = int(scale / 2)
+    pad = ((npad, npad), (npad, npad))
+    image_padding = np.pad(image, pad, 'constant', constant_values=(128))
+    x, y = image.shape
+    filter_arr = np.zeros(shape=(x + 2 * npad, y + 2 * npad))
+    for i in range(npad, x + npad):
+        for j in range(npad, y + npad):
+            for k in range(0, scale):
+                for s in range(0, scale):
+                    filter_arr[i, j] = filter_arr[i, j] + (mask[k, s]) * image_padding[i - npad + k, j - npad + s]
+    #여백제거
+    for i in range(1, npad+1):
+        filter_arr=np.delete(filter_arr,0,axis=0)
+        filter_arr = np.delete(filter_arr, 0, axis=1)
+
+        m,n=filter_arr.shape
+        filter_arr = np.delete(filter_arr, m-1, axis=0)
+        filter_arr = np.delete(filter_arr, n-1, axis=1)
+
+    return filter_arr
 
 def EdgeDetection(image):
     #grayscale
@@ -28,33 +52,25 @@ def EdgeDetection(image):
     #                [7 / 273, 26 / 273, 41 / 273, 26 / 273, 7 / 273],
     #                [4 / 273, 16 / 273, 28 / 273, 16 / 273, 4 / 273],
     #                [1 / 273, 4 / 273, 7 / 273, 4 / 273, 1 / 273]])
-    #가우시안 필터 생성
+
+    #가우시안 마스크 생성 및 적용
     scale = 5
-    mask2 = gaussian(scale, 2)
+    gaussian_mask = gaussian(scale, 1)
+    gaussian_arr=filter(gaussian_mask,scale,grayscale)
 
-    #패딩
-    npad = int(scale / 2)
-    pad = ((npad, npad), (npad, npad))
-    image_padding = np.pad(grayscale, pad, 'constant', constant_values=(128))
+    #라플라시안 마스크 및 적용
+    laplacian1_mask=np.array([[-1,-1,-1],
+                              [-1,8,-1],
+                              [-1,-1,-1]])
+    laplacian_mask2 = np.array([[0, 0, -1, 0, 0],
+                                [0, -1, -2, -1, 0],
+                                [-1, -2, 16, -2, -1],
+                                [0, -1, -2, -1, 0],
+                                [0, 0, -1, 0, 0]])
 
-    #가우시안 필터 적용
-    x, y = grayscale.shape
-    gaussian_arr = np.zeros(shape=(x + 2 * npad, y + 2 * npad))
-    for i in range(npad, x + npad):
-        for j in range(npad, y + npad):
-            for k in range(0, scale):
-                for s in range(0, scale):
-                    gaussian_arr[i, j] = gaussian_arr[i, j] + (mask2[k, s]) * image_padding[i - npad + k, j - npad + s]
+    laplacian_arr=filter(laplacian_mask2,5,gaussian_arr)
 
-    #여백 제거
-    for i in range(1, npad+1):
-        gaussian_arr=np.delete(gaussian_arr,0,axis=0)
-        gaussian_arr = np.delete(gaussian_arr, 0, axis=1)
-
-        m,n=gaussian_arr.shape
-        gaussian_arr = np.delete(gaussian_arr, m-1, axis=0)
-        gaussian_arr = np.delete(gaussian_arr, n-1, axis=1)
-
-    image = qimage2ndarray.array2qimage(gaussian_arr, normalize=False)
+    image = qimage2ndarray.array2qimage(laplacian_arr, normalize=False)
     qPixmapVar = QPixmap.fromImage(image)
     return qPixmapVar
+
